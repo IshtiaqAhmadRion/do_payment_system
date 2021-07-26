95e0b429c31476456c36a7e564de796794078669
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-07-26 15:08:56 +0600

    Update dropDownProvidert.dart

--- lib/providers/dropDownProvidert.dart
+++ lib/providers/dropDownProvidert.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 class DropDownProvider extends ChangeNotifier {
   List<String> _items = ['20 GP', '40 HC'];
   late String selectedItem;
-  String _initialValue = 'Select One';
+  
 
   List<String> get items => _items;
   String get selectd => selectedItem;
-  String get initValue => _initialValue;
+ 
 
   void setSelectedItem(String s) {
     selectedItem = s;
